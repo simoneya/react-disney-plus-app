d9f67d56c2483991eb8f2d763eca4ed748641abf
simoneya <yavar.simona@yahoo.com>
2022-11-20 12:53:36 +0000

    making navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,19 @@
 import styled from "styled-components";
 
 const Header = (props) => {
-    return <Nav>Header</Nav>;
+    return (
+    <Nav>
+        <Logo>
+            <img src="/images/logo.svg" alt="Disney+" />
+        </Logo>
+        <NavMenu>
+            <a href ="/home">
+                <img src = "/images/home-icon.svg" alt="HOME" />
+                <span>HOME</span>
+            </a> 
+        </NavMenu>
+    </Nav>
+  );
 }
 
 const Nav = styled.nav`
@@ -20,4 +32,68 @@ letter-spacing: 16px;
 z-index: 3;
 `;
 
+const Logo = styled.a`
+padding: 0;
+width: 60px;
+magin-top:  4px;
+max-height: 70px;
+font-size: 0;
+display: inline-block;
+    img {
+        display: block;
+        width: 100%;
+    }
+`;
+
+const NavMenu = styled.div`
+align-items: center;
+display: flex;
+flex-flow: row nowrap;
+height: 100%;
+justify-content: flex-end;
+margin: 0px;
+padding: 0px;
+position: relative;
+margin-right: auto;
+margin-left: 25px;
+
+a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+      img {
+          height: 20px;
+          max-width: 20px;
+          width: 20px;
+          z-index: auto;
+      }
+
+      span {
+          color: rgb(249, 249, 249);
+          font-size: 13px;
+          letter-spacing: 1.42px;
+          line-heihgt: 1.08;
+          padding: 2px 0px;
+          white-space: nowrap;
+          position: relative;
+      }
+
+      &:before {
+          background-color: rgb(249, 249, 249);
+          border-radius: 0px 0px 4px 4px;
+          bottom: -6px;
+          content: '';
+          opacity: 0;
+          position: absolute;
+          right: 0px;
+          transform-origin: left center;
+      }
+}
+
+${'' /* @media (max-widht: 768px) {
+    display: none;
+} */}
+`;
+
+ 
 export default Header;
