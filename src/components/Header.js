fbf6cb472cab4acabcb66d486badc8c89cbcdc63
simoneya <yavar.simona@yahoo.com>
2022-11-20 15:16:00 +0000

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,7 @@ const Header = (props) => {
                 <span>MOVIES</span>
             </a> 
         </NavMenu>
+        <Login>Login</Login>
     </Nav>
   );
 }
@@ -123,11 +124,27 @@ a {
         }
     }
 }
-
-${'' /* @media (max-widht: 768px) {
-    display: none;
-} */}
 `;
 
+
+        const Login = styled.a`
+        background-color: rgba(0, 0, 0, 0.6);
+        padding: 8px 16px;
+        text-transform: uppercase;
+        letter-spacing: 1.5px;
+        border: 1px solid #f9f9f9;
+        border-radius: 4px;
+        transition: all 0.2s ease 0s;
+
+            &:hover {   
+                background-color: #f9f9f9;
+                color: #000;
+                border-color: transparent;
+            }
+    `;
+
+
+
+
  
-export default Header;
+        export default Header;
