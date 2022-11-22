aeebeca71a81647b2d2d9dc6d873b8ff6cc13fa1
simoneya <yavar.simona@yahoo.com>
2022-11-22 20:32:09 +0000

    added viewers component

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -99,10 +99,14 @@ margin-yop: 20px;
             img {
                 width: 100%;
                 height: 100%;
-                
-
             }
 
+                &:hover {
+                    padding: 0;
+                    border: 4px solid rgba(249, 249, 249, 0.8);
+                    transition-duration: 300ms;
+
+                }
         }
   `;
 
