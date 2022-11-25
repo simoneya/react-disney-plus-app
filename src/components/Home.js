77d381646a9d5a97d5609c1578baa31c34e51b4e
simoneya <yavar.simona@yahoo.com>
2022-11-25 19:40:23 +0000

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,37 +12,79 @@ import db from "../firebase";
 import { setMovies } from "../features/movie/movieSlice";
 import { selectUserName } from "../features/user/userSlice";
 
-
-
-const Home =  (props) => {
+const Home = (props) => {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trending = [];
+  
+    useEffect(() => {
+      console.log("hello");
+      db.collection("movies").onSnapshot((snapshot) => {
+        snapshot.docs.map((doc) => {
+          console.log(recommends);
+          switch (doc.data().type) {
+            case "recommend":
+              recommends = [...recommends, { id: doc.id, ...doc.data() }];
+              break;
+  
+            case "new":
+              newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+              break;
+  
+            case "original":
+              originals = [...originals, { id: doc.id, ...doc.data() }];
+              break;
+  
+            case "trending":
+              trending = [...trending, { id: doc.id, ...doc.data() }];
+              break;
+          }
+        });
+  
+        dispatch(
+          setMovies({
+            recommend: recommends,
+            newDisney: newDisneys,
+            original: originals,
+            trending: trending,
+          })
+        );
+      });
+    }, [userName]); 
+    //we want this to be updated when username is updated(login) grab data(movies-data) 
+    //from firestore and store it in our redux store.//
+  
     return (
-    <Container>
+      <Container>
         <ImgSlider />
         <Viewers />
         <Recommends />
         <NewDisney />
         <Originals />
         <Trending />
-    </Container>
+      </Container>
     );
-};
-
-const Container = styled.main`
-position: relative;
-min-height: calc(100vh - 250px);
-overflow-x: hidden;
-display: block;
-top: 72px;
-padding: 0 calc(3.5vw + 5px);
-
+  };
+  
+  const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw + 5px);
     &:after {
-        background: url("/images/home-background.png") center center / cover no-repeat fixed;
-        content: '';
-        position: absolute;
-        inset: 0px;
-        opacity: 1;
-        z-index: -1;
-
+      background: url("/images/home-background.png") center center / cover
+        no-repeat fixed;
+      content: "";
+      position: absolute;
+      inset: 0px;
+      opacity: 1;
+      z-index: -1;
     }
-`;
-export default Home;
+  `;
+  
+  export default Home;
