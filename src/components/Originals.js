b55319657237e3e66435ff51270c90977f6d03bf
simoneya <yavar.simona@yahoo.com>
2022-11-26 10:40:07 +0000

    update

--- src/components/Originals.js
+++ src/components/Originals.js
@@ -1,61 +1,54 @@
+
 import styled from "styled-components";
- import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectOriginal } from "../features/movie/movieSlice";
 
- const Originals = (props) => {
-     return (
-         <Container>
-             <h4>Originals</h4>
-             <Content>
-                 <Wrap>
-                     <Link to="/">
-                         <img src="" alt="" />
-                     </Link>
+const Originals = (props) => {
+  const movies = useSelector(selectOriginal);
 
-                     <Link to="/">
-                         <img src="" alt="" />
-                     </Link>
+  return (
+    <Container>
+      <h4>Originals</h4>
+      <Content>
+        {movies &&
+          movies.map((movie, key) => (
+            <Wrap key={key}>
+              {movie.id}
+              <Link to={`/detail/` + movie.id}>
+                <img src={movie.cardImg} alt={movie.title} />
+              </Link>
+            </Wrap>
+          ))}
+      </Content>
+    </Container>
+  );
+};
 
-                     <Link to="/">
-                         <img src="" alt="" />
-                     </Link>
+const Container = styled.div`
+  padding: 0 0 26px;
+`;
 
-                     <Link to="/">
-                         <img src="" alt="" />
-                     </Link>
-                 </Wrap>
-             </Content>
-         </Container>
-     );
- };
+const Content = styled.div`
+  display: grid;
+  grid-gap: 25px;
+  gap: 25px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+`;
 
-  const Container = styled.div`
-    padding: 0 0 26px;
-
-  `;
-
-  const Content = styled.div`
-    display: grid;
-    grid-gap: 25px;
-    gap: 25px;
-    grid-template-columns: repeat(4, minmax(0, 1fr)); /*on desktop 4 columns */
-
-        @media (max-width: 768px) {
-            grid-template-columns: repeat(2, minmax(0, 1fr)); /*on mobile device 2 columns */
-        }
-  `;
-
-  const Wrap = styled.div`
-    padding-top: 56.25%;
+const Wrap = styled.div`
+  padding-top: 56.25%;
   border-radius: 10px;
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-
   cursor: pointer;
   overflow: hidden;
   position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
-  
   img {
     inset: 0px;
     display: block;
@@ -74,7 +67,6 @@ import styled from "styled-components";
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
   }
-  `;
+`;
 
-
- export default Originals;
+export default Originals;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
+    <Provider store={store}>
       <App />
-      </Provider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
